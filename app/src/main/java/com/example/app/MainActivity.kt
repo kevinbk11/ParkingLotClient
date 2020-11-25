75663c026f12396fd61b66556ced3576a60f2411
r91090701 <r91090701@gmail.com>
2020-11-26 00:51:35 +0800

    V2 done need test

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -7,66 +7,96 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.lang.Exception
 import java.net.Socket
 
-class MainActivity : AppCompatActivity() {
-    val SocketClient=SocketThread()
+class MainActivity : AppCompatActivity()
+{
+    val SocketClient = SocketThread()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         SocketClient.start()
-        btm1.visibility=View.GONE
-        Thread{
-                while(true)
+        btm1.visibility = View.GONE
+        textView1.visibility = View.GONE
+        Thread {
+            while (true)
+            {
+                Thread.sleep(1000)
+                if (!SocketClient.Connecting)
                 {
-                    Thread.sleep(1000)
-                    if(!SocketClient.Connecting)
-                    {
-                        break
-                    }
+                    break
                 }
-            if(SocketClient.Notfail)
+            }
+            if (SocketClient.Notfail)
             {
                 runOnUiThread {
-                    btm1.visibility=View.VISIBLE
-                    Toast.makeText(applicationContext,"連線成功",Toast.LENGTH_LONG).show()
+                    btm1.visibility = View.VISIBLE
+                    textView1.visibility = View.VISIBLE
+                    textView2.visibility = View.GONE
+                    Toast.makeText(applicationContext, "連線成功", Toast.LENGTH_LONG).show()
+
                 }
 
             }
             else
             {
-                runOnUiThread{Toast.makeText(applicationContext,"連線失敗! 有可能是伺服器未開啟或者網路不穩,請重新嘗試!",Toast.LENGTH_LONG).show()}
+                runOnUiThread {
+                    Toast.makeText(applicationContext, "連線失敗! 有可能是伺服器未開啟或者網路不穩,請重新嘗試!", Toast.LENGTH_LONG).show()
+                }
             }
         }.start()
     }
 
     fun click(view: View)
     {
-
-        if(SocketClient.Notfail)
+        btm1.visibility=View.GONE
+        if (SocketClient.Notfail)
         {
-            Toast.makeText(applicationContext,"已成功傳送車牌號碼,請至出口稍等",Toast.LENGTH_LONG).show()
+            Toast.makeText(applicationContext, "已成功傳送車牌號碼,請至出口稍等", Toast.LENGTH_LONG).show()
         }
-        Thread{
-            SocketClient.sendCar(editText.text.toString())
+        Thread {
+            var number = SocketClient.sendCar(editText.text.toString())
+            while(true)
+            {
+
+                if(number?.minus(1)==0)
+                {
+                    runOnUiThread {
+                        textView1.text="正在將您的愛車移動至出口,請稍後"
+                    }
+                    break
+                }
+                else
+                {
+                    runOnUiThread {
+                        textView1.text="您前面還有${number?.minus(1)}位客人,請耐心等候"
+                    }
+                }
+                number=SocketClient.GetNumber()
+            }
+
         }.start()
-        }
     }
+}
 
 
 class SocketThread():Thread()
 {
     var writer : PrintWriter? = null
+    var reader : BufferedReader?=null
     var Notfail:Boolean=false
     var Connecting:Boolean=true
     override fun run()
     {
         try {
-            print("test")
             val s= Socket("192.168.43.201",5005)
             val output=s.getOutputStream()
+            val input = s.getInputStream()
+            reader= BufferedReader(InputStreamReader(input))
             writer= PrintWriter(output,true)
             Notfail=true
         }
@@ -80,10 +110,14 @@ class SocketThread():Thread()
         }
     }
 
-    fun sendCar(car:String)
+    fun sendCar(car:String):Int?
     {
         writer?.println(car)
+        return reader?.readLine()?.toInt()
     }
 
-
+    fun GetNumber():Int?
+    {
+        return reader?.readLine()?.toInt()
+    }
 }
